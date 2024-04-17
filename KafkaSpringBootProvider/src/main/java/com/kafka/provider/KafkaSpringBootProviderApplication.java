c155f37a02c284cc2dca98a4dc755508471ce683
Rattle-Brain <danialgo2001@gmail.com>
2024-04-17 12:52:16 +0200

    App base upload

---
+++ KafkaSpringBootProvider/src/main/java/com/kafka/provider/KafkaSpringBootProviderApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringBootProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringBootProviderApplication.class, args);
+	}
+
+}
