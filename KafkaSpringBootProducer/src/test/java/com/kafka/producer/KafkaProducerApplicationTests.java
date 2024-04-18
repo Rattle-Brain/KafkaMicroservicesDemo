4e77ed935e60d684fbe797310c914543085e5b2a
Rattle-Brain <danialgo2001@gmail.com>
2024-04-18 13:19:50 +0200

    Renaming another class

--- KafkaSpringBootProducer/src/test/java/com/kafka/producer/KafkaProducerApplicationTests.java
+++ KafkaSpringBootProducer/src/test/java/com/kafka/producer/KafkaProducerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KafkaSpringBootProviderApplicationTests {
+class KafkaProducerApplicationTests {
 
 	@Test
 	void contextLoads() {
