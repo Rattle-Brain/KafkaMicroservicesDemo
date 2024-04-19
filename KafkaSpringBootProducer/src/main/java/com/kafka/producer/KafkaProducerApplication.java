442b79552aeb892e5d1bb9cf9af25753629d2685
Rattle-Brain <danialgo2001@gmail.com>
2024-04-19 12:14:22 +0200

    User input works

--- KafkaSpringBootProducer/src/main/java/com/kafka/producer/KafkaProducerApplication.java
+++ KafkaSpringBootProducer/src/main/java/com/kafka/producer/KafkaProducerApplication.java
@@ -3,16 +3,22 @@ package com.kafka.producer;
 import com.kafka.producer.config.KafkaTopicConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class KafkaProducerApplication {
 
 	@Autowired
 	private KafkaAdmin kafkaAdmin;
+
 	@Autowired
 	private KafkaTopicConfig ktc;
 
@@ -21,15 +27,33 @@ public class KafkaProducerApplication {
 	}
 
 	@Bean
-	public NewTopic topic1() {
-		ktc = new KafkaTopicConfig();
-		return ktc.createTopic("this-new-topic", 3, KafkaTopicConfig.DEFAULT_REPLICATION_FACTOR);
+	CommandLineRunner init (KafkaTemplate<String, String> kt){
+		return args -> {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Enter your message (type 'exit' to quit):");
+			while (true) {
+				String input = reader.readLine();
+				if (input.equalsIgnoreCase("exit")) {
+					break;
+				}
+				kt.send("default-topic", input);
+				System.out.println("Message sent to Kafka: " + input);
+			}
+			System.out.println("Exiting Kafka Producer Application...");
+		};
 	}
 
 	@Bean
+	public NewTopic topic1() {
+		ktc = new KafkaTopicConfig();
+		//return ktc.createTopic("this-new-topic".toCharArray(), (short)3, KafkaTopicConfig.DEFAULT_REPLICATION_FACTOR);
+		return ktc.generateDefaultTopic();
+	}
+/*
+	@Bean
 	public NewTopic topic2() {
 		ktc = new KafkaTopicConfig();
-		return ktc.createTopic("kafka-games-topic", 2, KafkaTopicConfig.DEFAULT_REPLICATION_FACTOR);
+		return ktc.createTopic("kafka-games-topic".toCharArray(), (short)2, KafkaTopicConfig.DEFAULT_REPLICATION_FACTOR);
 	}
-
+*/
 }
