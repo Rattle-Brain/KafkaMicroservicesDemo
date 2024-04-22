33e2af927a1bc984c454b20de5e56dbc3d5821f4
Rattle-Brain <danialgo2001@gmail.com>
2024-04-22 14:22:38 +0200

    Listener reads from all topics

--- KafkaSpringBootConsumer/src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
+++ KafkaSpringBootConsumer/src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
 
 @Configuration
 public class KafkaConsumerListener {
@@ -13,6 +14,10 @@ public class KafkaConsumerListener {
     @KafkaListener(topics = {"this-new-topic"}, groupId = "new-topic-groupid")
     public void listenerNew(String msg){
         LOGGER.info("Msg from topic \"this-new-topic\": " + msg);
+    }
+    @KafkaListener(id = "my-consumer", groupId = "new-topic-groupid", topicPattern = ".*")
+    public void listenToAllTopics(String message, @Header("kafka_receivedTopic") String topic) {
+        System.out.println("Msg from topic \"" + topic + "\": " + message);
     }
 /*
     @KafkaListener(topics = {"kafka-games-topic"}, groupId = "kafka-games-groupid")
