b0c96db5a42c81e836390832e68f26511147e641
Rattle-Brain <danialgo2001@gmail.com>
2024-05-07 13:18:11 +0200

    Removed @Conf tag

--- KafkaSpringBootProducer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
+++ KafkaSpringBootProducer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
@@ -12,7 +12,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+
 public class KafkaProducerConfig {
 
     @Value("${spring.kafka.bootstrapServers}")
