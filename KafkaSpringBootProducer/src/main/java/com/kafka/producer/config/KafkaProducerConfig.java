35be110bf0ef10c54bd5c7fbb4b883db9bd7c2e8
Rattle-Brain <danialgo2001@gmail.com>
2024-05-10 13:02:35 +0200

    Kafkaproducer

--- KafkaSpringBootProducer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
+++ KafkaSpringBootProducer/src/main/java/com/kafka/producer/config/KafkaProducerConfig.java
@@ -1,16 +1,16 @@
 package com.kafka.producer.config;
 
 import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 
 public class KafkaProducerConfig {
@@ -18,20 +18,25 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrapServers}")
     private String server;
 
-    public Map<String, Object> producerConfig(){
-        Map<String, Object> conf = new HashMap<>();
-        conf.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, server);
-        conf.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        conf.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return conf;
+    public Properties producerConfig(){
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                org.apache.kafka.common.serialization.StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                io.confluent.kafka.serializers.KafkaAvroSerializer.class);
+        props.put("schema.registry.url", "http://localhost:8081");
+
+        return props;
     }
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory(){
-        return new DefaultKafkaProducerFactory<>(producerConfig());
+    //@Bean
+    public KafkaProducer createProducer(){
+        Properties props = producerConfig();
+        return new KafkaProducer(props);
     }
 
-    @Bean
+    //@Bean
     public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
         return new KafkaTemplate<>(producerFactory);
     }
